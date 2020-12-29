432497a23499ea2cab3d2a4553b31aaea26cddbb
PajserDivizija <luka.pranjic@fer.hr>
2020-12-29 01:51:04 +0100

    Bugfixes

--- objectdetection/objectdetection.py
+++ objectdetection/objectdetection.py
@@ -28,14 +28,14 @@ def get_people_coordinates(image_path):
         exit()
     classes = None
 
-    image = plt.imread(image_path)
+    image = cv2.imread(image_path)
     height, width, ch = image.shape
     with open(COCO_NAMES, 'r') as f:
         classes = [line.strip() for line in f.readlines()]
 
     # read pre-trained model and config file
     net = cv2.dnn.readNet(YOLOV3_WEIGHTS, YOLOV3_CFG)
-    net.setInput(cv2.dnn.blobFromImage(image, 1/255.0, (416,416), (0,0,0), True, crop=False))
+    net.setInput(cv2.dnn.blobFromImage(image, 0.00392, (416,416), (0,0,0), True, crop=False))
     layer_names = net.getLayerNames()
     output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
     outs = net.forward(output_layers)
@@ -63,11 +63,13 @@ def get_people_coordinates(image_path):
                 boxes.append([x, y, w, h])
 
     #finds the best-fitting box
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-
-
-    # im = Image.open(image_path) #opening input picture for drawing
-    # draw_im = ImageDraw.Draw(im)
+    
+    indices_1 = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.3)
+    indices_2 = cv2.dnn.NMSBoxes(boxes, confidences, 0, 0)
+    if len(indices_1) >= len(indices_2):
+        indices = indices_1
+    else:
+        indices = indices_2
 
     correct_boxes = []
     for i in indices:
@@ -75,9 +77,6 @@ def get_people_coordinates(image_path):
         box = boxes[i]
         if class_ids[i]==0:
             x,y,w,h = box
-            correct_boxes.append([x,y,x+w,y+h])
-            # draw_im.rectangle([x,y,x+w,y+h],outline=color_generator(),width=3)
-    
-    # im.show()
+            correct_boxes.append([int(x),int(y),int(x+w),int(y+h)])
     
     return correct_boxes
